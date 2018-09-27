2415518c53cd3ee159c110c1cd776127d7f5ec59
Shaya Gopin <sgopin@gmail.com>
2018-09-27 02:42:04 -0400

    Added Models

---
+++ SemesterManagement/Models/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SemesterManagement.Models.Enums;
+
+namespace SemesterManagement.Models
+{
+    public class Entity
+    {
+        public long EntityID { get; set; }
+        public EntityType Type { get; set; }
+    }
+}
---
+++ SemesterManagement/Models/Semester.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SemesterManagement.Models
+{
+    public class Semester
+    {
+        public long SemesterID { get; set; }
+        public string Name { get; set; }
+        public string Desription { get; set; }
+        public Entity Entity { get; set; }
+        public Template Template { get; set; }
+    }
+}
